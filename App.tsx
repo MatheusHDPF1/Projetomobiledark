15c60e2b1b7909ab5993702ed3e67e6a21b7e84b
Matheus <Matheus@gmail.com>
2021-11-10 12:00:26 -0300

    Adição de Stores

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 import { estilo } from './css/Styles';
 import { Image } from 'react-native';
 
@@ -8,7 +8,11 @@ export default function App() {
   return (
     <View style={estilo.container}>
 
+
+      {/* {Inicio do Header} */}
+
       <View style={estilo.header}>
+
         <View style={estilo.perfil}>
           <Image source={{uri:"https://st.depositphotos.com/2101611/3925/v/600/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture.jpg"}} style={estilo.imgperfil}/> 
       </View>
@@ -19,11 +23,75 @@ export default function App() {
       </View>
 
       <View style={estilo.pesquisa}>
-        <Image source={{uri:"https://w7.pngwing.com/pngs/321/177/png-transparent-computer-icons-magnifier-magnifying-glass-magnifying-glass-text-share-icon-magnifier.png"}} style={estilo.imgpesquisa}/>
+        <Image source={{uri:"https://upload.wikimedia.org/wikipedia/meta/thumb/7/7e/Vector_search_icon.svg/945px-Vector_search_icon.svg.png"}} style={estilo.imgpesquisa}/>
       
       </View>
       </View>
-      <StatusBar style="auto" />
+      {/* {Fim do Header} */}
+
+      {/* {Inicio do Stores} */}
+
+      <View style={estilo.stories}>
+         <Text style={estilo.titulostories}>
+           Stories
+         </Text>
+
+         <ScrollView horizontal={true} style={estilo.scrollstories}>
+           <View>
+             <Text style={estilo.blackbox}>Live</Text>
+              <Image source={{uri:"https://images.unsplash.com/photo-1635454022133-e287f3924950?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"}} style={estilo.imgstories}/>
+              
+              <View style={estilo.txticon}>
+                <Text style={estilo.txt}>Savannah</Text>
+                <Image source={require("./assets/iconverifiedcópia.png")} style={estilo.icon}/>
+
+              </View>
+           </View>
+
+           <View>
+           <Text style={estilo.redbox}>Live</Text>
+              <Image source={{uri:"https://images.unsplash.com/photo-1635876988521-35f66cda80ab?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"}} style={estilo.imgstories}/>
+              
+              <View style={estilo.txticon}>
+                <Text style={estilo.txt}>Cooper</Text>
+                <Image source={require("./assets/iconverifiedcópia.png")} style={estilo.icon}/>
+
+              </View>
+           </View>
+
+           <View>
+              <Image source={{uri:"https://images.unsplash.com/photo-1633950291913-41d5727b9b73?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=704&q=80"}} style={estilo.imgstories}/>
+              
+              <View style={estilo.txticon}>
+                <Text style={estilo.txt}>Howard</Text>
+                <Image source={require("./assets/iconverifiedcópia.png")} style={estilo.icon}/>
+
+              </View>
+           </View>
+
+           <View>
+              <Image source={{uri:"https://images.unsplash.com/photo-1635672978235-a5677e4a6131?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=687&q=80"}} style={estilo.imgstories}/>
+              
+              <View style={estilo.txticon}>
+                <Text style={estilo.txt}>Woman</Text>
+                <Image source={require("./assets/iconverifiedcópia.png")} style={estilo.icon}/>
+
+              </View>
+           </View>
+
+           <View>
+              <Image source={{uri:"https://images.unsplash.com/photo-1634423623074-de676f545acd?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1587&q=80"}} style={estilo.imgstories}/>
+              
+              <View style={estilo.txticon}>
+                <Text style={estilo.txt}>Man</Text>
+                <Image source={require("./assets/iconverifiedcópia.png")} style={estilo.icon}/>
+
+              </View>
+           </View>
+          </ScrollView>
+
+      </View>
+      {/* {Fim do Stores #####} */}
     </View>
   );
 }
--- css/Styles.tsx
+++ css/Styles.tsx
@@ -7,7 +7,7 @@ export const estilo = StyleSheet.create({
     },
     header:{
         flexDirection:'row',
-        paddingTop:20,
+        paddingTop:60,
         paddingLeft:10,
         paddingRight:20,
         paddingBottom:30,
@@ -25,23 +25,21 @@ export const estilo = StyleSheet.create({
 
     },
     nome:{
-        color:'989cac',
+        color:'#989cac',
         fontWeight:'bold',
-        fontSize:20,
+        fontSize:18,
         padding:10
 
     },
     descricao:{
-        color:'989cac',
+        color:'#989cac',
         paddingLeft:20,
-
     },
     imgpesquisa:{
         flex:1,
         width:30,
         height:50,
         resizeMode:'contain'
-
     }, 
     pesquisa:{
 
@@ -49,6 +47,68 @@ export const estilo = StyleSheet.create({
     titulo:{
         flex:2
 
+    },
+    stories:{
+
+
+    },
+    titulostories:{
+        color:'#989cac',
+        fontSize:20,
+        margin:20
+    },
+    scrollstories:{
+
+    },
+    imgstories:{
+        width:200,
+        height:300,
+        resizeMode:'contain',
+        borderRadius:50,
+        margin:9
+    },
+    blackbox:{
+        backgroundColor:'#090909',
+        color:'#989cac',
+        fontSize:15,
+        padding:10,
+        width:50,
+        borderRadius:9,
+        position:"absolute",
+        zIndex:1000,
+        top:20,
+        left:20,
+        textAlign:'center',
+        
+    },
+    redbox:{
+        backgroundColor:'#F5635D',
+        color:'#000000',
+        fontSize:15,
+        padding:9,
+        width:99,
+        borderRadius:8,
+        position:"absolute",
+        zIndex:1000,
+        top:24,
+        left:25,
+        textAlign:'center',
+    },
+    txticon:{
+        flexDirection:"row",
+        alignItems:'center'
+
+    },
+    txt:{
+        color:"#989cac",
+        fontSize:17,
+        padding:10
+    },
+    icon:{
+        width:18,
+        height:15,
+        resizeMode:'contain'
     }
+    
 
 })
